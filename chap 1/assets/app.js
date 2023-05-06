ea48ca07ff04a38432fa798101187f85ce87f9f9
Faiqa-batool <115587465+Faiqa-batool@users.noreply.github.com>
2023-05-06 22:13:46 +0500

    Add files via upload

---
+++ chap 1/assets/app.js
@@ -0,0 +1,27 @@
+// Question 1:
+alert("Welcome to My Website!")
+
+// Question 2:
+alert("Error! Please enter a valid password.")
+
+// Question 3:
+alert("Welcome to JS Land... \nHappy Coding!")
+
+// Question 4:
+setTimeout(function() {
+    alert("Welcome to JS Land...");
+    setTimeout(function() {
+      alert("Happy Coding!");
+    }, 1000); 
+    // wait 1 second before displaying the second message
+  }, 1000);
+   // wait 1 second before displaying the first message
+
+// Question 5:
+console.log("Hello... I can run JS through my web browser's cosole")
+
+// Question 6:
+// practiced it
+
+// Question 7:
+// practiced it
---
+++ chap 2/assets/app.js
@@ -0,0 +1,41 @@
+// Question 1:
+var username;
+
+// Question 2:
+var myName = "Faiqa Batool";
+
+// Question 3:
+var message = "Hello World";
+alert(message);
+
+// Question 4:
+// Student's Bio Data saving in variables
+let var1 = "Faiqa Batool";
+let var2 = 20;
+let var3 = "Certified Mobile Application Developer";
+
+// Showing Student's Bio data in Alert box
+alert(var1);
+alert(var2);
+alert(var3);
+
+// Question 5:
+var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(message);
+
+// Question 6:
+var email = "faiqa.b48@gmail.com";
+alert("My email address is " + email);
+
+// Question 7:
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book "+ book);
+
+// Question 9:
+let strVar = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(strVar);
+
+// Question 8:
+document.write("Yah! I can write HTML content through JavaScript");
+
+
